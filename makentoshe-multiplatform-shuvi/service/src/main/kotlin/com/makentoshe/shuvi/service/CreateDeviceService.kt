e2638094426d40ad240763a553d19a6bbd975119
Makentoshe <mkliverout@gmail.com>
2022-02-14 21:28:28 +0300

    feat: change CreateDeviceService routing

--- makentoshe-multiplatform-shuvi/service/src/main/kotlin/com/makentoshe/shuvi/service/CreateDeviceService.kt
+++ makentoshe-multiplatform-shuvi/service/src/main/kotlin/com/makentoshe/shuvi/service/CreateDeviceService.kt
@@ -2,5 +2,5 @@ package com.makentoshe.shuvi.service
 
 interface CreateDeviceService: Service {
     override val routing: String
-        get() = "/device/create.json"
+        get() = "api/v1/device/create"
 }
