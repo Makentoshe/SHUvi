b98dbf49b12adaf5828d9506d0f879c960cd8f91
Makentoshe <mkliverout@gmail.com>
2022-02-16 15:14:21 +0300

    refactor: change CreatedDeviceResponse package and module

--- makentoshe-multiplatform-shuvi/entity/src/main/kotlin/com/makentoshe/shuvi/entity/CreatedDevice.kt
+++ makentoshe-multiplatform-shuvi/entity/src/main/kotlin/com/makentoshe/shuvi/entity/CreatedDevice.kt
@@ -1,7 +1,3 @@
 package com.makentoshe.shuvi.entity
 
-import com.makentoshe.shuvi.common.Either
-
-typealias CreatedDeviceResponse = Either<CreatedDevice, Exception>
-
 class CreatedDevice(val device: Device, val isSuccess: Boolean)
---
+++ makentoshe-multiplatform-shuvi/repository/src/main/kotlin/com/makentoshe/shuvi/entity/CreatedDeviceResponse.kt
@@ -0,0 +1,5 @@
+package com.makentoshe.shuvi.entity
+
+import com.makentoshe.shuvi.common.Either
+
+typealias CreatedDeviceResponse = Either<CreatedDevice, Exception>
