3f8feba4ac2f8b700e4edc83bb1f48b0f7b0ac5a
Makentoshe <mkliverout@gmail.com>
2022-02-16 17:52:09 +0300

    feat: implement database response for devices request

---
+++ makentoshe-multiplatform-shuvi/database/src/main/kotlin/com/makentoshe/shuvi/response/database/DatabaseGetAllDeviceResponse.kt
@@ -0,0 +1,6 @@
+package com.makentoshe.shuvi.response.database
+
+import com.makentoshe.shuvi.common.Either
+import com.makentoshe.shuvi.entity.database.DatabaseDevice
+
+typealias DatabaseGetDevicesResponse = Either<List<DatabaseDevice>, Exception>
