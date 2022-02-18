65f86128dae54185a09ff40fb281237f4e2efa26
Makentoshe <mkliverout@gmail.com>
2022-02-18 17:46:21 +0300

    feat: implement database insert for sensors

--- makentoshe-multiplatform-shuvi/database-mongo/src/main/kotlin/com/makentoshe/shuvi/database/sensor/MongoInsertSensorDatabase.kt
+++ makentoshe-multiplatform-shuvi/database-mongo/src/main/kotlin/com/makentoshe/shuvi/database/sensor/MongoInsertSensorDatabase.kt
@@ -1,6 +1,7 @@
 package com.makentoshe.shuvi.database.sensor
 
 import com.makentoshe.shuvi.common.Either
+import com.makentoshe.shuvi.entity.database.DatabaseInsertedSensor
 import com.makentoshe.shuvi.entity.database.DatabaseSensor
 import com.makentoshe.shuvi.response.database.DatabaseInsertedSensorResponse
 import com.mongodb.client.MongoCollection
@@ -8,7 +9,12 @@ import com.mongodb.client.MongoCollection
 internal class MongoInsertSensorDatabase(
     private val collection: MongoCollection<DatabaseSensor>,
 ) : InsertSensorDatabase {
-    override fun insertSensor(sensor: DatabaseSensor): DatabaseInsertedSensorResponse {
-        return Either.Right(Exception("$sensor should be inserted here"))
+    override fun insertSensor(sensor: DatabaseSensor): DatabaseInsertedSensorResponse = try {
+        val insertOneResult = collection.insertOne(sensor)
+        if (!insertOneResult.wasAcknowledged()) throw Exception("Insert wasn't acknowledged")
+
+        Either.Left(DatabaseInsertedSensor(sensor))
+    } catch (exception: Exception) {
+        Either.Right(exception)
     }
 }
