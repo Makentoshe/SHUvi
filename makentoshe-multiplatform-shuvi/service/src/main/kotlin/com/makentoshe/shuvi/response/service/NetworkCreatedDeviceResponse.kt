b5e0b25b511409b7a8af234413d7249ad1203053
Makentoshe <mkliverout@gmail.com>
2022-02-19 14:39:00 +0300

    fix: fix NetworkCreatedDeviceResponse#Failure: add additional constructor for case when device wasn't created at all

--- makentoshe-multiplatform-shuvi/service/src/main/kotlin/com/makentoshe/shuvi/response/service/NetworkCreatedDeviceResponse.kt
+++ makentoshe-multiplatform-shuvi/service/src/main/kotlin/com/makentoshe/shuvi/response/service/NetworkCreatedDeviceResponse.kt
@@ -10,24 +10,26 @@ import kotlinx.serialization.Serializable
 @Serializable
 sealed class NetworkCreatedDeviceResponse {
 
-    /** Device that should be created */
-    abstract val device: NetworkDevice
-
     @Serializable
     data class Success(
-        override val device: NetworkDevice,
+        val device: NetworkDevice,
     ) : NetworkCreatedDeviceResponse() {
         constructor(createdDevice: CreatedDevice) : this(NetworkDevice(createdDevice.device))
     }
 
     @Serializable
     data class Failure(
-        override val device: NetworkDevice,
         val exception: String,
         val message: String,
+        val device: NetworkDevice? = null,
     ) : NetworkCreatedDeviceResponse() {
         constructor(device: NetworkDevice, exception: Exception) : this(
             device = device,
+            exception = exception.javaClass.simpleName,
+            message = exception.message ?: "",
+        )
+
+        constructor(exception: Exception) : this(
             exception = exception.javaClass.simpleName,
             message = exception.message ?: "",
         )
