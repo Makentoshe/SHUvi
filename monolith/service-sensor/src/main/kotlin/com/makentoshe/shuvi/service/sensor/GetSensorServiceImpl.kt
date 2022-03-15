61cbcd616ff5e388265afbe4ff7d7cdb7882f563
Makentoshe <mkliverout@gmail.com>
2022-03-15 13:38:35 +0300

    feat: refactor get sensor service

--- monolith/service-sensor/src/main/kotlin/com/makentoshe/shuvi/service/sensor/GetSensorServiceImpl.kt
+++ monolith/service-sensor/src/main/kotlin/com/makentoshe/shuvi/service/sensor/GetSensorServiceImpl.kt
@@ -1,20 +1,20 @@
 package com.makentoshe.shuvi.service.sensor
 
+import com.makentoshe.shuvi.common.flatMapLeft
 import com.makentoshe.shuvi.entity.SensorId
-import com.makentoshe.shuvi.entity.service.NetworkSensor
 import com.makentoshe.shuvi.entity.service.NetworkSensor2
 import com.makentoshe.shuvi.repository.sensor.GetSensorRepository
 import com.makentoshe.shuvi.response.service.sensor.NetworkGetSensorResponse
-import io.ktor.application.*
-import io.ktor.http.*
-import io.ktor.response.*
+import com.makentoshe.shuvi.service.ServiceCall
+import com.makentoshe.shuvi.service.parameter
+import io.ktor.http.HttpStatusCode
 
 class GetSensorServiceImpl(private val getSensorRepository: GetSensorRepository) : GetSensorService {
-    override suspend fun handle(call: ApplicationCall) {
-        val sensorId = call.parameters[sensorIdParameter]
-            ?: return call.respond(HttpStatusCode.BadRequest, "TODO handle sensor id parameter issue.")
 
-        getSensorRepository.id(SensorId(sensorId)).bimap({ sensor ->
+    override suspend fun handle(call: ServiceCall) {
+        call.parameter(sensorIdParameter).flatMapLeft { sensorIdString ->
+            getSensorRepository.id(SensorId(sensorIdString))
+        }.bimap({ sensor ->
             NetworkGetSensorResponse.Success(NetworkSensor2(sensor))
         }, { exception ->
             NetworkGetSensorResponse.Failure(exception)
